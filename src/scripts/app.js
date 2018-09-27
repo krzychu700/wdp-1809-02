67003b162dbfef7b907ccacbcd813ea1a3ecb46c
endriu17 <aolaszewski@gmail.com>
2018-09-27 22:31:33 +0200

    fix code reviev for BUG WDP-1809-2

--- src/scripts/app.js
+++
@@ -1,6 +0,0 @@
-var selectOption = document.querySelector('header .menu-bar .search-form .category select');
-
-selectOption.addEventListener('mouseover', function(){
-    console.log('mouse over');
-    selectOption.style.backgroudColor = '#d58e32;'
-})
