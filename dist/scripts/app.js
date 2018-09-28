7f2dbd85052f966eb3f4756ac9b5579010474e31
endriu17 <aolaszewski@gmail.com>
2018-09-28 16:35:59 +0200

    fix BUG WDP180902-5 styles for favorite and compare buttons

---
+++ dist/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var btnFavorite = document.querySelectorAll('a.btn-outline');
+
+for (var i = 0; i < btnFavorite.length; i++) {
+	btnFavorite[i].addEventListener('click', function (event) {
+        var classActive = this.classList;
+        if (classActive.contains('active')){
+            this.classList.remove('active');
+        } else {
+            this.className += ' active';
+        }
+    }, false);
+    
+}
