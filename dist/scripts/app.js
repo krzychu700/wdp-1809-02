c8af1436f0a1d08d5586d7b8034997fa075ce43a
endriu17 <aolaszewski@gmail.com>
2018-10-02 23:14:05 +0200

    fix js script apperance

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // stars rating functions
+
 var starsContener = document.querySelectorAll('.stars');
 
 for (var i = 0; i < starsContener.length; i++){
@@ -8,33 +9,35 @@ for (var i = 0; i < starsContener.length; i++){
     starsContener[i].addEventListener('click', setRating);
     starsContener[i].addEventListener('mouseover', setHover);
     starsContener[i].addEventListener('mouseout', removeHover);
+    
     function setRating(e){
         let cStar = e.target;
         let stars = this.querySelectorAll('.star');
         let match = false;
         stars.forEach(function(star){
-            if(match){
+            if (match) {
                 star.classList.remove('full');
-            }else{
+            } else {
                 star.classList.add('full');
                 star.style.color = '#d58e32';
             }
-            if(star === cStar){
+            if (star === cStar) {
                 match = true;
             }
         });
     }
+
     function setHover(e){
         let hStar = e.target;
         let stars = this.querySelectorAll('.star');
         let match = false;
         stars.forEach(function(star){
-            if(match){
+            if (match) {
                 star.classList.remove('star-hover');
-            }else{
+            } else {
                 star.classList.add('star-hover');
             }
-            if(star === hStar){
+            if (star === hStar){
                 match = true;
             } 
         });
