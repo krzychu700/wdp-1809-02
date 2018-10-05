893fdab0e467cae1e61ac66fe545ad0e719ab4b9
Krzysztof Szypulski <krzychu700@gmail.com>
2018-10-05 19:21:42 +0200

    created dynamic dots script

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -1,10 +1,11 @@
-(function(){
+// (function () {
 var productsCard = document.querySelectorAll(".js-products");
 var jsBed = document.getElementsByClassName("bed-products");
 var jsChair = document.getElementsByClassName("chair-products");
 var jsSofa = document.getElementsByClassName("sofa-products");
 var jsTable = document.getElementsByClassName("table-products");
 var jsDining = document.getElementsByClassName("dining-products");
+var jsDoots = document.getElementById("js-dots");
 
 function cardChecker() {
     if (productsCard[0].classList.contains("active")) {
@@ -34,14 +35,59 @@ function cardChecker() {
     }
 }
 
+function dotsCreator() {
+    var elemCreator = document.createElement("li");
+    var linkCreator = document.createElement("a");
+    var child = jsDoots.appendChild(elemCreator);
+    child.appendChild(linkCreator);
+    child.setAttribute("class", "dynamic-dot");
+}
+
+function dotsCounter() {
+    var jsDynamicDots = document.querySelectorAll(".dynamic-dot");
+    for (var l = 0; l < jsDynamicDots.length; l++) {
+        jsDoots.removeChild(jsDynamicDots[l]);
+    }
+}
+
+function dotsChecker() {
+    var bedProducts = jsBed[0].querySelectorAll(".product-box").length;
+    var chairProducts = jsChair[0].querySelectorAll(".product-box").length;
+    if ((bedProducts > 8) && (productsCard[0].classList.contains("active"))) {
+        do {
+            dotsCreator();
+            bedProducts - 8;
+        }
+        while (bedProducts < 8);
+    } else {
+        dotsCounter();
+    };
+    if ((chairProducts > 8) && (productsCard[1].classList.contains("active"))) {
+        do {
+            dotsCreator();
+            chairProducts = chairProducts - 8;
+            console.log(chairProducts);
+        }
+        while (chairProducts < 8);
+    } else {
+        dotsCounter();
+    }
+}
+
+// dotsChecker();
+
 for (var i = 0; i < productsCard.length; i++) {
 
     productsCard[i].addEventListener("click", function () {
+
         for (var j = 0; j < productsCard.length; j++) {
             productsCard[j].classList.remove("active");
         };
         this.className += " active";
         cardChecker();
+        dotsCounter();
+        dotsChecker();
     });
 }
-})();
+
+// })();
